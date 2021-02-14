e6210578959343d04489e247afe4b802fb34c75e
corytveten <corytveten@gmail.com>
2021-02-13 16:31:51 -0800

    initial db migration

--- textbook-spa-backend/config/routes.rb
+++ textbook-spa-backend/config/routes.rb
@@ -1,3 +1,6 @@
 Rails.application.routes.draw do
+  resources :textbooks
+  resources :courses
+  resources :schools
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
---
+++ textbook-spa-backend/db/migrate/20210214002837_create_textbooks.rb
@@ -0,0 +1,12 @@
+class CreateTextbooks < ActiveRecord::Migration[5.2]
+  def change
+    create_table :textbooks do |t|
+      t.string :name
+      t.string :author
+      t.string :edition
+      t.references :course, foreign_key: true
+
+      t.timestamps
+    end
+  end
+end
