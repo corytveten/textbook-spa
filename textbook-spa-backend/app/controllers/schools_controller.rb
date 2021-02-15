365dff5042ed00eaa2de64bb7bdab7789bd3f4f5
corytveten <corytveten@gmail.com>
2021-02-14 19:31:34 -0800

    school serializer working

--- textbook-spa-backend/app/controllers/schools_controller.rb
+++ textbook-spa-backend/app/controllers/schools_controller.rb
@@ -2,11 +2,11 @@ class SchoolsController < ApplicationController
     
     def index
         schools = School.all
-        render json: schools.to_json(:include => {:courses => {:only => [:code, :title]}}, :except => [:created_at, :updated_at])   
+        render json: SchoolSerializer.new(schools).to_serialized_json
     end
 
     def show
         school = School.find_by(id: params[:id])
-        render json: school.to_json(:include => {:courses => {:only => [:code, :title]}}, :except => [:created_at, :updated_at])  
+        render json: SchoolSerializer.new(school).to_serialized_json
     end
 end
--- textbook-spa-backend/app/services/school_serializer.rb
+++ textbook-spa-backend/app/services/school_serializer.rb
@@ -1,2 +1,11 @@
 class SchoolSerializer
+     
+  def initialize(school_object)
+    @school = school_object
+  end
+ 
+  def to_serialized_json
+    @school.to_json(:include => {:courses => {:only => [:code, :title]}}, :except => [:created_at, :updated_at])  
+  end
+ 
 end
