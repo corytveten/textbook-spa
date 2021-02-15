75af8ea7d5df10e1774bfe18a3a40f33c5500bfa
corytveten <corytveten@gmail.com>
2021-02-14 19:14:34 -0800

    create serializer

--- textbook-spa-backend/app/controllers/schools_controller.rb
+++ textbook-spa-backend/app/controllers/schools_controller.rb
@@ -2,11 +2,11 @@ class SchoolsController < ApplicationController
     
     def index
         schools = School.all
-        render json: schools    
+        render json: schools.to_json(:include => {:courses => {:only => [:code, :title]}}, :except => [:created_at, :updated_at])   
     end
 
     def show
         school = School.find_by(id: params[:id])
-        render json: school
+        render json: school.to_json(:include => {:courses => {:only => [:code, :title]}}, :except => [:created_at, :updated_at])  
     end
 end
