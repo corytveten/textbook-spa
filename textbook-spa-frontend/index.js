e982e142ddbc02f3a69a496f38667c3b387d7717
corytveten <corytveten@gmail.com>
2021-02-14 18:41:47 -0800

    fetch schools

--- textbook-spa-frontend/index.js
+++ textbook-spa-frontend/index.js
@@ -1,10 +1,31 @@
-//console.log("hello")
 
 const BASE_URL = "http://localhost:3000";
 const SCHOOLS_URL = `${BASE_URL}/schools`;
 const COURSES_URL = `${BASE_URL}/courses`;
-const TEXTBOOKS_URL = `${BASE_URL}/textbooks`
+const TEXTBOOKS_URL = `${BASE_URL}/textbooks`;
+const schoolList = document.querySelector('.school-list')
 
 document.addEventListener('DOMContentLoaded', () => {
     loadSchools()
 })
+
+//fetch schools
+const loadSchools = () => {
+    fetch(SCHOOLS_URL)
+    .then(res => res.json())
+    .then(json => {
+        json.forEach(schoolObj => {
+
+            renderSchools(schoolObj)
+        })
+    })
+}
+
+const renderSchools = (schoolObj) => {
+    //create JS Object for school?
+    console.log(schoolObj);
+    const schoolLi = document.createElement('li');
+    schoolLi.classList.add('school');
+    schoolLi.innerText = schoolObj.name;
+    schoolList.appendChild(schoolLi);
+}
