018ababea503dc364caec7ddb50b46e9654cc4bf
corytveten <corytveten@gmail.com>
2021-02-14 18:23:37 -0800

    add html and js variables

--- textbook-spa-frontend/index.js
+++ textbook-spa-frontend/index.js
@@ -1,1 +1,10 @@
-console.log("hello")
+//console.log("hello")
+
+const BASE_URL = "http://localhost:3000";
+const SCHOOLS_URL = `${BASE_URL}/schools`;
+const COURSES_URL = `${BASE_URL}/courses`;
+const TEXTBOOKS_URL = `${BASE_URL}/textbooks`
+
+document.addEventListener('DOMContentLoaded', () => {
+    loadSchools()
+})
