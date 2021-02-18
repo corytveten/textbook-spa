851aab137f79afecf43f29b2eba85a476c05accc
corytveten <corytveten@gmail.com>
2021-02-17 19:32:18 -0800

    post fetch successful for schools

--- textbook-spa-backend/app/controllers/schools_controller.rb
+++ textbook-spa-backend/app/controllers/schools_controller.rb
@@ -9,4 +9,15 @@ class SchoolsController < ApplicationController
         school = School.find_by(id: params[:id])
         render json: SchoolSerializer.new(school).to_serialized_json
     end
+
+    def create
+        school = School.create(school_params)
+        render json: school        
+    end
+
+    #may want to add strong params
+    private
+    def school_params
+        params.permit(:name)
+    end
 end
