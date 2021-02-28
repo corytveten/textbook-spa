229a08d0910e6845e56f714e11d23fd3b007846f
corytveten <corytveten@gmail.com>
2021-02-27 16:29:30 -0800

    textbook listing and form order correct

--- textbook-spa-frontend/src/course.js
+++ textbook-spa-frontend/src/course.js
@@ -11,20 +11,25 @@ class Course {
 
     createCourseLi() {
 
-        const nodeToAppendCourses = document.querySelector(`[data-school-id='${this.schoolId}'] div div`);
+        const nodeToAppendCourses = document.querySelector(`[data-school-id='${this.schoolId}']`);
 
         const courseLi = document.createElement('li');
         const courseSpan = document.createElement('span');
+        const courseTextbookInfo = document.createElement('div');
+        const textbooks = document.createElement('div');
     
         courseLi.classList.add('course');
         courseSpan.classList.add('course-span');
+        courseTextbookInfo.classList.add('course-textbook-info');
         courseLi.setAttribute('course-data-id', this.id);
+        textbooks.classList.add('textbooks');
     
         courseSpan.innerText = `${this.code} - ${this.title}`;
-        //console.log(courseLi)
 
         nodeToAppendCourses.appendChild(courseLi);
+        nodeToAppendCourses.appendChild(courseTextbookInfo);
         courseLi.appendChild(courseSpan);
+        courseTextbookInfo.appendChild(textbooks);
     }
 
     createTextbookForm() {
@@ -32,6 +37,7 @@ class Course {
         //debugger;
         //console.log(this)
         const courseNode = document.querySelector(`[course-data-id='${this.id}']`);
+        console.log(courseNode.nextElementSibling)
         //const courseNode = document.querySelector(`[data-school-id='${this.schoolId}'] div div [course-data-id=]`);
         console.log(courseNode);
         const addTextbookDiv = document.createElement('div');
@@ -39,13 +45,13 @@ class Course {
         addTextbookDiv.classList.add('add-text');
         addTextbookDiv.innerHTML =
             `<h4>Add a Textbook</h4>
-                <form textbook-form-data-id='${this.course_id}'>
-                    <input type="text" name='title' value='' placeholder="Enter Textbook Title" textbook-title-data-input-id="${this.course_id}">
-                    <input type='text' name='author' value='' placeholder='Enter Author Last Name' textbook-author-data-input-id='${this.course_id}'>
+                <form textbook-form-data-id='${this.id}'>
+                    <input type="text" name='title' value='' placeholder="Enter Textbook Title" textbook-title-data-input-id="${this.id}">
+                    <input type='text' name='author' value='' placeholder='Enter Author Last Name' textbook-author-data-input-id='${this.id}'>
                     <input id='textbook-btn' type="submit" name="submit" value="Submit">
                 </form>`
 
-        courseNode.append(addTextbookDiv);
+        courseNode.nextElementSibling.append(addTextbookDiv);
         //submitTextbook(this.course_id)
         
     }
